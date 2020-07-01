6f21ae81ef649c889de6b2a45dc598f503ac081f
Katherine Jones <kejone1414@gmail.com>
2020-06-30 21:15:14 -0400

    feat: html routes working

---
+++ routes/api-routes.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const Workout = require("../models/workout");
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .sort({ date: -1 })
+        .limit(1)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.updateOne({ id: req.params.id }, { req })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -10,12 +10,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 app.use(require("./routes/api-routes"));
+require("./routes/html-routes.js")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
